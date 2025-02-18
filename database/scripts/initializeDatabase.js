5062a6572dd455bc0198d3ee15e8be3c1f86169d
arthur <artyr007@gmail.com>
2025-02-18 20:59:21 +0500

    feat(misc): add awaitCustomWebElements client script to mitigate FOUCE and slightly adjusted script loading

---
+++ database/scripts/initializeDatabase.js
@@ -0,0 +1,2 @@
+// We create tables here,
+// maybe assign default values and so on
---
+++ public/scripts/main.js
@@ -0,0 +1,19 @@
+const awaitCustomWebComponents = async () => {
+  const definedElements = new Set();
+  const allElements = document.querySelectorAll("*");
+
+  const promises = Array.from(allElements).map((element) => {
+    const tagName = element.tagName.toLowerCase();
+    if (tagName.includes("-")) {
+      if (!definedElements.has(tagName)) {
+        definedElements.add(tagName);
+        return customElements.whenDefined(tagName);
+      }
+    }
+  });
+
+  await Promise.allSettled(promises);
+  document.body.classList.add("ready");
+};
+
+document.addEventListener("DOMContentLoaded", awaitCustomWebComponents);
